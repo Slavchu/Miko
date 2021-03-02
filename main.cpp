9ff205f23e312be4d57b319fbc14713f6f6a683d
bogdan-chernenko <bogdan.chernenko.018@gmail.com>
2021-03-02 20:27:28 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+/*#include "MikoStruct.h"
+#include "API/Telegram/TelegramAPI.h"
+#include "API/Instagram/InstagramApi.h"
+#include "API/Discord/DiscordApi.h"
+#include "API/Vk/VkApi.h"
+
+#include "Read/ReadMikoFile.h"
+
+
+*/
+int main(int argc, char *argv[])
+{
+    std::fstream OpenMikoFile("main.miko");
+
+    std::string *numbers = new std::string[];
+    std::string MikoFile = "";
+    std::string t = "";
+
+    while (!OpenMikoFile.eof())
+    {
+        getline(OpenMikoFile, t);
+        MikoFile += "\n";
+        MikoFile += t;
+    }
+
+
+    std::cout << MikoFile << std::endl;
+
+    return 0;
+}
